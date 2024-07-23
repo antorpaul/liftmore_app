a4c280a5f660c748c4caa71108d6115d37924db3
Antor Paul <antorp@uci.edu>
2024-07-23 07:33:35 -0700

    - Update page to start including categories. WIP, currently broken

--- liftmore_web/api/exercise.ts
+++ liftmore_web/api/exercise.ts
@@ -7,9 +7,10 @@ export interface Exercise {
     category_id: number
 }
 
-export const fetchAllExercises = async () : Promise<Exercise[]> => {
+export const fetchAllExercises = async (page: number = -1, page_size: number = -1, category_id: number = -1) : Promise<Exercise[]> => {
   try {
-      const response = await fetch(`${urlBase.base}${urlBase.exercise}s/all`);
+      const response = await fetch(`${urlBase.base}${urlBase.exercise}s/all?page=${page}&page_size=${page_size}&category_id=${category_id}`);
+      console.log(`${urlBase.base}${urlBase.exercise}s/all?page=${page}&page_size=${page_size}&category_id=${category_id}`);
       if (!response.ok) {
           throw new Error("Network response was invalid.");
       }
--- liftmore_web/app/(ui)/exercises/page.tsx
+++ liftmore_web/app/(ui)/exercises/page.tsx
@@ -15,8 +15,9 @@ import { Input } from '@/components/ui/input';
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 import {Textarea} from "@/components/ui/textarea";
 
-async function RetrieveAllExercises() {
-    const exercises: Exercise[] = await fetchAllExercises();
+async function RetrieveAllExercises(page: number = -1, page_size: number = -1, category_id: number = -1) {
+    const exercises: Exercise[] = await fetchAllExercises(page, page_size, category_id)
+
     return (
       <div>
           <ul>
@@ -37,6 +38,14 @@ async function RetrieveAllExercises() {
     );
 }
 
+// async function RetrieveAllCategories() {
+//     const categories: Category[] = await fetchAllCategories();
+//
+//     return (
+//
+//     )
+// }
+
 export default function ExerciesMain() {
     return (
         <main>
@@ -44,40 +53,56 @@ export default function ExerciesMain() {
                 <h1 className="libre-franklin-h1 text-xl">Exercise Library</h1>
             </div>
             <div className="flex px-2 pb-4">
-                <h3 className="libre-franklin-h3">Don&apos;t see what you&apos;re looking for?</h3>
-                <Dialog>
-                    <DialogTrigger>
-                        <Button variant="outline">Create New</Button>
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px]">
-                        <DialogHeader>
-                            <div className="flex align-middle">
-                                <TbPencil size="20" className="mt-[0.1em] mr-2"/>
-                                <h1 className="libre-franklin-h1">Create Exercise</h1>
+                <div className="max-w-96">
+                    <Select>
+                        <SelectTrigger>
+                            <SelectValue placeholder="Select Exercise Category"></SelectValue>
+                        </SelectTrigger>
+                        <SelectContent>
+                            {/* TODO Implement <RetrieveAllCategories /> */}
+                            {/*<SelectItem value="bicep">Bicep Exercises</SelectItem>*/}
+                            {/*<SelectItem value="leg">Leg Exercises</SelectItem>*/}
+                            {/*<SelectItem value="calf">Calf Exercises</SelectItem>*/}
+                            {/*<SelectItem value="chest">Chest Exercises</SelectItem>*/}
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div className="flex items-center ml-auto">
+                    <h3 className="libre-franklin-h3 pr-2">Don&apos;t see what you&apos;re looking for?</h3>
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button variant="outline">Create New</Button>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-[425px]">
+                            <DialogHeader>
+                                <div className="flex align-middle">
+                                    <TbPencil size="20" className="mt-[0.1em] mr-2"/>
+                                    <h1 className="libre-franklin-h1">Create Exercise</h1>
+                                </div>
+                            </DialogHeader>
+                            <div className="grid grid-cols-1 gap-y-2">
+                                <Input type="text" placeholder="Exercise Name" />
+                                <Select>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Select Exercise Category"></SelectValue>
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="bicep">Bicep Exercises</SelectItem>
+                                        <SelectItem value="leg">Leg Exercises</SelectItem>
+                                        <SelectItem value="calf">Calf Exercises</SelectItem>
+                                        <SelectItem value="chest">Chest Exercises</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                                <Textarea placeholder="Describe your category." />
                             </div>
-                        </DialogHeader>
-                        <div className="grid grid-cols-1 gap-y-2">
-                            <Input type="text" placeholder="Exercise Name" />
-                            <Select>
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Select Exercise Category"></SelectValue>
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectItem value="bicep">Bicep Exercises</SelectItem>
-                                    <SelectItem value="leg">Leg Exercises</SelectItem>
-                                    <SelectItem value="calf">Calf Exercises</SelectItem>
-                                    <SelectItem value="chest">Chest Exercises</SelectItem>
-                                </SelectContent>
-                            </Select>
-                            <Textarea placeholder="Describe your category." />
-                        </div>
-                        <DialogFooter>
-                            <Button type="submit">Save Changes</Button>
-                        </DialogFooter>
-                    </DialogContent>
-                </Dialog>
+                            <DialogFooter>
+                                <Button type="submit">Save Changes</Button>
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
+                </div>
             </div>
-            <RetrieveAllExercises />
+            {/* TODO Get retrieve all exercises working with categories and pagination <RetrieveAllExercises />*/}
         </main>
     );
 }
